eb4addc36bada904612385724dd17c482d4fa443
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-03-16 12:59:44 +0200

    friends front block

---
+++ sites/all/themes/profect/templates/views/partner/views-view-fields--partners--front-block.tpl.php
@@ -0,0 +1,12 @@
+<div class="partner-title col-sm-3">
+    <div class="partner-title_header">
+       <?php print $fields['field_image']->content; ?>
+    </div>
+    <div class="text is-small partner-title_description">
+        <p> <?php print $fields['field_body']->content; ?></p>
+    </div>
+    <div class="partner-title_link">
+          
+            
+    </div>
+</div>

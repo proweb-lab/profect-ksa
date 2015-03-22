a76e8be20dbc7ea3f9a0badba3a93edd3914eafc
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-03-22 16:20:53 +0200

    update 22/3

--- sites/all/themes/profect/templates/page.tpl.php
+++ sites/all/themes/profect/templates/page.tpl.php
@@ -1,61 +1,53 @@
 
 <header id="navbar" role="banner">
-  <div class="container">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-      <?php endif; ?>
- 
+    <div class="container">
+        <div class="navbar-header">
+            <?php if ($logo): ?>
+                <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+                    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+                </a>
+            <?php endif; ?>
+        </div>
+        <?php print render($page['header']); ?>
     </div>
-      <?php print render($page['header']); ?>
-  </div>
- 
+    <?php if (!empty($page['highlighted'])): ?>
+        <div class="main-menu"><?php print render($page['highlighted']); ?></div>
+    <?php endif; ?>
 </header>
 
-        
-<div class="main-container container">
-       <?php if (!empty($page['highlighted'])): ?>
-        <div class="main-menu"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-        <div class="full-width front-slider content-wrap">
-         <?php if (drupal_is_front_page()): ?>
+<?php if (drupal_is_front_page()): ?>
+    <div class="full-width front-slider content-wrap">
         <?php print views_embed_view('slider', 'front_slider'); ?>
-        <?php endif; ?>
-        </div>
-          <div class="front-text content-wrap">
-         <?php if (drupal_is_front_page()): ?>
-        <?php print views_embed_view('front_text', 'block'); ?>
-        <?php endif; ?>
-        
-   </div>
+    </div>
+<?php endif; ?>
+
 <?php print render($title_prefix); ?>
-        <?php if (!empty($title)): ?>
-        <h1 class="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-        <?php print render($title_suffix); ?>
-  <div class="row">
-      
-
-    <section<?php print $content_column_class; ?>>
-      
-      <a id="main-content"></a>
+<?php if (!empty($title)): ?>
+    <div class="page-title"><h1><?php print $title; ?></h1></div>
+<?php endif; ?>
+<?php print render($title_suffix); ?>
     
-      
-      
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-    </section>
-
-  </div>
+<div class="main-container container">
+        <?php if (drupal_is_front_page()): ?>
+            <div class="front-text content-wrap">
+                <?php print views_embed_view('front_text', 'block'); ?>
+            </div>
+        <?php endif; ?>  
+        <div class="row">
+            <div<?php print $content_column_class; ?>>
+                <?php print $messages; ?>
+                <?php if (!empty($tabs)): ?>
+                    <?php print render($tabs); ?>
+                <?php endif; ?>
+                <?php if (!empty($action_links)): ?>
+                    <ul class="action-links"><?php print render($action_links); ?></ul>
+                <?php endif; ?>
+                <?php print render($page['content']); ?>
+            </div>
+        </div>
 </div>
+
+
 <footer class="footer container">
-  <?php print render($page['footer']); ?>
+    <?php print render($page['footer']); ?>
 </footer>
---
+++ sites/all/themes/profect/templates/views/product/views-view-fields--products--page.tpl.php
@@ -0,0 +1,13 @@
+<div class="entry clearfix">
+    <div class="entry_image">
+        <?php print $fields['field_images']->content; ?>
+    </div>
+    <div class="entry_c">
+        <div class="entry_title"><h2><?php print $fields['title']->content; ?></h2></div>
+        <div class="clear"></div>
+        <span class="page-divider"><span></span></span>
+        <div class="entry_content">
+            <p><?php print $fields['field_body']->content; ?></p>
+        </div>
+    </div>
+</div>
